aef5bd1f43d1a2e3a28ac97fa61dc507a514ef09
ArthurArthurArthur0817 <102396327+ArthurArthurArthur0817@users.noreply.github.com>
2024-03-13 17:01:01 +0800

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -8,18 +8,18 @@ union_set = set()
 for continent in df.columns:
     union_set.update(df[continent])
 
-print("聯集:", union_set)
+print("各洲所有常用社群媒體(聯集):", union_set)
 
 # 交集
 intersection_set = set(df[df.columns[0]])
 for continent in df.columns[1:]:
     intersection_set.intersection_update(set(df[continent]))
 
-print("交集:", intersection_set)
+print("各洲都在用的社群媒體(交集):", intersection_set)
 
 # 差集
 difference_set = set(df[df.columns[0]])
 for continent in df.columns[1:]:
     difference_set.difference_update(set(df[continent]))
 
-print("差集:", difference_set)
+print("只有部分洲使用的社群媒體(差集):", difference_set)
